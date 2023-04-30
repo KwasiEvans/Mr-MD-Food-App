9b61c254f837ca8d42458ac877771ddfa720db55
Evans Owusu <evansow2002@gmail.com>
2023-04-29 15:02:03 -1200

    initial commit

---
+++ frontend/lib/screens/home/main_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello world"),
+    );
+  }
+}
