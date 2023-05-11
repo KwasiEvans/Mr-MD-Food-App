818f7bbe125180789b792eb82e46953077eab659
Evans Owusu <evansow2002@gmail.com>
2023-05-10 20:09:18 -1200

    completed address Model and created some variables in the location controller

--- frontend/lib/controllers/location_controller.dart
+++ frontend/lib/controllers/location_controller.dart
@@ -1,4 +1,5 @@
 import 'package:frontend/data/repository/location_repo.dart';
+import 'package:frontend/models/address_model.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
@@ -12,5 +13,8 @@ class LocationController extends GetxController implements GetxService {
   late Position _pickPosition;
   Placemark _placemark = Placemark();
   Placemark _pickPlacemark = Placemark();
-  
+  List<AddressModel> _addressList = [];
+  late List<AddressModel> _allAddressList;
+  List<String> _addressTypeList = ["home", "office", "others"];
+  int _addressTypeIndex = 0;
 }
